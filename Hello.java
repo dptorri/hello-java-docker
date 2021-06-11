577e1f47747e720932fae45b36733f801846db50
Daniel Torri <daniel.torri@kmon.net>
2021-06-11 21:33:08 +0200

    Add simple list of files in tmp folder

--- Hello.java
+++ Hello.java
@@ -1,5 +1,15 @@
-class Hello{
-   public static void main(String args[]){
+import java.io.*;
+import java.util.*;
+
+public class Hello {
+   public static void main(String args[]) throws IOException {
+      String pathString = System.getProperty("java.io.tmpdir");
+      String [] fileNames = new File(pathString).list();
       System.out.println("HELLO");
+      System.out.println("==============================");
+      System.out.println("Your 5 first files on tmp folder");
+      for (int i = 0; i < 5; i++) {
+         System.out.println(fileNames[i]);
+      }
    }
 }
